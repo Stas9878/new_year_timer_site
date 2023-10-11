b02337628a451330fe4b9a98827c649854c3ce11
Popov Stas <boss.staspopov@mail.ru>
2023-10-11 21:45:29 +0300

    Добавил логику для входа и регистрации

--- new_year_timer/accounts/urls.py
+++ new_year_timer/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    # path('login/', login, name='login'),
 ]
--- new_year_timer/accounts/views.py
+++ new_year_timer/accounts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
-def register(request):
-    pass
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+# def login(request):
+#     return render(request, 'accounts/login.html')
 
 
